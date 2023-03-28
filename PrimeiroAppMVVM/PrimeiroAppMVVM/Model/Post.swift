c525e44bbfad1357f5c26049391d55ddb7c8f65e
Felipe Angeli <felipe.angeli@gmail.com>
2023-03-27 22:21:49 -0300

    final modulo

---
+++ PrimeiroAppMVVM/PrimeiroAppMVVM/Model/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  PrimeiroAppMVVM
+//
+//  Created by Felipe  on 13/03/23.
+//
+
+import Foundation
+
+struct Posts {
+    let profileImage: String
+    let userName: String
+    let postImage: String
+}
