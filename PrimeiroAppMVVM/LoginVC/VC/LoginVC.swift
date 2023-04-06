b7e59708df4b318f64f307fa7a0f7453b00abae2
Felipe Angeli <felipe.angeli@gmail.com>
2023-04-05 21:37:14 -0300

    Firebase

---
+++ PrimeiroAppMVVM/LoginVC/VC/LoginVC.swift
@@ -0,0 +1,85 @@
+//
+//  LoginVC.swift
+//  PrimeiroProjetoViewCode
+//
+//  Created by Felipe  on 03/04/23.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    private var loginScreen: LoginScreen?
+    private var viewModel: LoginViewModel = LoginViewModel()
+    
+    
+    override func loadView() {
+        loginScreen = LoginScreen()
+        view = loginScreen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginScreen?.delegate(delegate: self)
+        loginScreen?.configTextFieldDelegate(delegate: self)
+        viewModel.delegate(delegate: self)
+    }
+
+
+}
+
+extension LoginVC: LoginScreenProtocol {
+    func tappedLoginButton() {
+        print("chegou na VC")
+        viewModel.login(email: loginScreen?.emailTextField.text ?? "", password: loginScreen?.passwordTextField.text ?? "")
+    }
+ 
+}
+
+extension LoginVC: LoginViewModelProtocol {
+    func successLogin() {
+        let vc: HomeVC = HomeVC()
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
+    
+    func errorLogin(errorMessage: String) {
+        print(#function)
+        Alert(controller: self).showAlertInformation(title: "Ops, error Login!", message: errorMessage)
+    }
+    
+    
+}
+
+
+
+extension LoginVC: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+        let email: String = loginScreen?.emailTextField.text ?? ""
+        let passwordTextField: String = loginScreen?.passwordTextField.text ?? ""
+        
+        if !email.isEmpty && !passwordTextField.isEmpty {
+            print("Botao habilitado")
+            loginScreen?.loginButton.isEnabled = true
+            loginScreen?.loginButton.backgroundColor = .darkGray
+        } else {
+            print("Botao desabilitado")
+            loginScreen?.loginButton.isEnabled = false
+            loginScreen?.loginButton.backgroundColor = .darkGray.withAlphaComponent(0.6)
+        }
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        textField.resignFirstResponder()
+        return false
+    }
+    
+}
---
+++ PrimeiroAppMVVM/LoginVC/ViewModel/LoginViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewModel.swift
+//  PrimeiroAppMVVM
+//
+//  Created by Felipe  on 05/04/23.
+//
+
+import UIKit
+import Firebase
+
+protocol LoginViewModelProtocol: AnyObject{
+    func successLogin()
+    func errorLogin(errorMessage: String)
+}
+
+class LoginViewModel {
+    
+    private weak var delegate: LoginViewModelProtocol?
+    public func delegate(delegate: LoginViewModelProtocol?) {
+        self.delegate = delegate
+    }
+
+    private var auth = Auth.auth()
+    
+    public func login(email: String, password: String) {
+        auth.signIn(withEmail: email, password: password) { authResult , error in
+            if error == nil {
+                print("sucesso login")
+                self.delegate?.successLogin()
+            } else {
+                print("Error login, error: \(error?.localizedDescription ?? "")")
+                self.delegate?.errorLogin(errorMessage: error?.localizedDescription ?? "")
+            }
+        }
+    }
+    
+    public func registerUser(email: String, password: String) {
+        auth.createUser(withEmail: email, password: password) { authResult , error in
+            if error == nil {
+                print("sucesso cadastro")
+                
+            } else {
+                print("Error cadastro, error: \(error?.localizedDescription ?? "")")
+            }
+        }
+    }
+    
+}
